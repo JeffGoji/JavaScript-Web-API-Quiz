14eb99ad7cd497364f439aee66ff08ff42e11387
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-09-17 21:36:24 -0500

    questions created for quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+// A variable containing every question and choice and answer.
+let questions = [
+  {
+    title: "How do you call a function named myFunction?",
+    choices: ["myFunction()", "functionMy()", "call,myFunction()"],
+    answer: "myFunction()",
+  },
+  {
+    title: "Inside which HTML element do you put the JavaScript?",
+    choices: [
+      "<HEAD> element",
+      "<BODY> element",
+      "<DIV> element",
+      "<BODY> and <HEAD> elements",
+    ],
+    answer: "<BODY> and <HEAD> elements",
+  },
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<scripting>", "<js>", "<javaScript>", "<script>"],
+    answer: "<script>",
+  },
+  {
+    title: "What is the type of Pop up boxes available in JavaScript?:",
+    choices: ["Alert", "Confirm", "Prompt", "All the above"],
+    answer: "All the above",
+  },
+  {
+    title: "How to write an IF statement in JavaScript?",
+    choices: ["if i = 5", "if (i == 5)", "if i = 5 then", "if i == 5"],
+    answer: "if (i == 5)",
+  },
+];
